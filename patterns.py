b98933b26812f0ffe5cb21856e7f8507ee58172a
Sebastian <sebastian.rs.ba@gmail.com>
2024-11-22 17:52:26 -0300

    feat: found movies by pattern and organize files

--- patterns.py
+++ patterns.py
@@ -8,3 +8,8 @@ m3u_channel_pattern = re.compile(
 m3u_channel_pattern = re.compile(
     r'#EXTINF:-1.*?tvg-id="(?P<tvg_id>[^"]+\.br)".*?tvg-name="(?P<channel>.+?)".*?tvg-logo="(?P<tvg_logo>[^"]+)".*?group-title="(?P<group_title>[^"]+)",(?P<channel_name>.+?)\n(?P<url>http[^\n]+)'
 )
+# Expressão regular para filmes
+movie_pattern = re.compile(
+    r'group-title="Filmes(?! \|.*(?:Adultos|[Xx]{3}.*Adultos)).*?",(?P<movie>.*?)\n(?P<url>http[^\n]+)',
+    flags=re.IGNORECASE,
+)
---
+++ processors/movie_processor.py
@@ -0,0 +1,37 @@
+import re
+import os
+from media_directory import MediaDirectory
+from patterns import (
+    movie_pattern,
+)
+
+
+class MovieProcessor(MediaDirectory):
+    def __init__(self, output_dir):
+        super().__init__(output_dir, "movie")
+
+    def process(self, text):
+        movie_matches = movie_pattern.finditer(text)
+        for match in movie_matches:
+            movie = match.group("movie").strip()
+            movie = re.sub(r"(:|/|-)", "", movie, flags=re.IGNORECASE)
+            movie = re.sub(r"(\D)4K", r"\1 4K", movie)
+            movie = re.sub(r"\s+", " ", movie)
+            url = match.group("url")
+            movie_dir = os.path.join(self.base_dir, movie)
+            os.makedirs(movie_dir, exist_ok=True)
+            strm_file = os.path.join(movie_dir, f"{movie}.strm")
+
+            if os.path.exists(strm_file):
+                with open(strm_file, "r") as f:
+                    old_url = f.read().strip()  # Lê o link anterior
+                print(f"Substituindo link anterior: {old_url}")
+            else:
+                old_url = None
+            with open(strm_file, "w") as f:
+                f.write(url)
+
+            if old_url:
+                print(f"Link do arquivo '{strm_file}' atualizado para: '{url}'")
+            else:
+                print(f"Criado arquivo '{strm_file}' com link: {url}")
