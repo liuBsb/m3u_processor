ac3cdaf38d4b5b21f620d5f7ef1eee33dbe7a89a
Sebastian <sebastian.rs.ba@gmail.com>
2024-11-22 21:34:52 -0300

    refact: rename test

--- tests/test_tv_channels_processor.py
+++ tests/test_tv_channels_processor.py
@@ -13,7 +13,9 @@ def m3u_file_mock():
 def test_process_tv_channel(m3u_file_mock):
     # Simulando a leitura do arquivo M3U
     with patch("builtins.open", mock_open(read_data=m3u_file_mock)):
-        with patch("sys.argv", ["main.py", "test.m3u", "-t", "tv_channel"]):
+        with patch(
+            "sys.argv", ["main.py", "test.m3u", "-t", "tv_channel", "-s", "tests"]
+        ):
             main()  # Chama a função main do script principal
         # Verifique se o arquivo M3U foi processado corretamente
         # Aqui você pode adicionar mais verificações específicas, como chamadas ao banco de dados
