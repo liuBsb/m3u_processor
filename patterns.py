468e92e1612c3439081ac6d610e386ac0a13e189
Sebastian <sebastian.rs.ba@gmail.com>
2024-11-22 21:24:14 -0300

    fix: update patterns

--- patterns.py
+++ patterns.py
@@ -1,7 +1,7 @@
 import re
 
 # Expressão regular para canais de TV m3u
-m3u_channel_pattern = re.compile(
+channel_pattern = re.compile(
     r'#EXTINF:-1.*?tvg-id="(?P<tvg_id>[^"]+\.br)".*?tvg-name="(?P<channel>.+?)".*?tvg-logo="(?P<tvg_logo>[^"]+)".*?group-title="(?P<group_title>[^"]+)",(?P<channel_name>.+?)\n(?P<url>http[^\n]+)'
 )
 # Expressão regular para séries
@@ -11,7 +11,9 @@ series_pattern = re.compile(
 )
 # Expressão regular para filmes
 movie_pattern = re.compile(
-    r'group-title="Filmes(?! \|.*(?:Adultos|[Xx]{3}.*Adultos)).*?",(?P<movie>.*?)\n(?P<url>http[^\n]+)',
+    r'group-title="(?:FILM|FILME|FILMES)(?! \|.*(?:ADULTO[S]?|[Xx]{3}.*ADULTO[S]?)).*?",'
+    r"(?P<movie>(?!.*(?:\b[Xx]{3}\b|FHD|H265|HD|4K|UHD|SD|[Cc]anal|[Ll]ive)).*?)\n"
+    r"(?P<url>http[^\n]+)",
     flags=re.IGNORECASE,
 )
 # Expressão regular para conteúdo adulto
