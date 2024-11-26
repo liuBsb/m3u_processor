f8dcf0bb8dc40861d98b0cc93a4158947568f02a
Sebastian <sebastian.rs.ba@gmail.com>
2024-11-25 22:46:32 -0300

    feat: now verify movie link for save

--- database.py
+++ database.py
@@ -1,36 +1,34 @@
 import os
 import sqlite3
-from sqlite3 import Connection  # Para tipagem
+import subprocess
+from sqlite3 import Connection
+from utils.config import Config
 
 
 class Database:
-    DEFAULT_DB_PATH = os.path.join(
-        os.path.expanduser("~"), ".m3u_processor", "media.db"
-    )
-
-    def __init__(self, db_path: str | None = None):
-        self.db_path: str = db_path or self.DEFAULT_DB_PATH
+    def __init__(self, db_path=None):
+        if db_path is None:
+            config = Config()
+            db_path = config.get_database_path(environment="development")
+        self.db_path = db_path
         self._ensure_directory_exists()
-        self.connection: Connection | None = None  # Anotação explícita de tipo
+        self.connection: Connection | None = None
 
     def _ensure_directory_exists(self):
         os.makedirs(os.path.dirname(self.db_path), exist_ok=True)
 
     def connect(self):
-        """Estabelece a conexão com o banco de dados."""
         if not self.connection:
             self.connection = sqlite3.connect(self.db_path)
 
     def close(self):
-        """Fecha a conexão com o banco de dados."""
         if self.connection:
             self.connection.close()
             self.connection = None
 
     def initialize(self):
-        """Cria as tabelas necessárias no banco de dados."""
         self.connect()
-        if self.connection:  # Garantindo que a conexão não é None
+        if self.connection:
             cursor = self.connection.cursor()
             cursor.execute(
                 """
@@ -44,21 +42,27 @@ class Database:
                 )
             """
             )
-            self.connection.commit()
-
-    def save(self, channel_data: dict):
-        """Salva os dados do canal no banco de dados."""
-        self.connect()
-        if self.connection:
-            cursor = self.connection.cursor()
             cursor.execute(
                 """
-                INSERT INTO tv_channels (channel_name, tvg_id, tvg_logo, group_title, url)
-                VALUES (:channel_name, :tvg_id, :tvg_logo, :group_title, :url)
-                """,
-                channel_data,
+                CREATE TABLE IF NOT EXISTS movies (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    title TEXT NOT NULL,
+                    url TEXT NOT NULL UNIQUE,
+                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                )
+            """
             )
             self.connection.commit()
+
+    def save(self, table: str, data: dict):
+        """Insere os dados no banco de dados."""
+        placeholders = ", ".join(f":{key}" for key in data.keys())
+        columns = ", ".join(data.keys())
+        query = f"INSERT INTO {table} ({columns}) VALUES ({placeholders})"
+        try:
+            self.execute(query, data)
+        except sqlite3.IntegrityError:
+            print(f"Registro já existe: {data['title']}")
 
     def execute(self, query: str, params: dict | list | None = None):
         """Executa uma consulta no banco de dados."""
@@ -73,7 +77,7 @@ class Database:
             return cursor
         raise RuntimeError("A conexão com o banco de dados não foi estabelecida.")
 
-    def fetchall(self, query: str, params: dict | list | None = None):
+    def fetchall(self, query: str, params: dict | list | tuple | None = None):
         """Executa uma consulta e retorna todos os resultados."""
         self.connect()
         if self.connection:
@@ -84,3 +88,18 @@ class Database:
                 cursor.execute(query)
             return cursor.fetchall()
         raise RuntimeError("A conexão com o banco de dados não foi estabelecida.")
+
+    @staticmethod
+    def verify_link(url: str) -> bool:
+        """Verifica se o link funciona usando ffmpeg."""
+        try:
+            result = subprocess.run(
+                ["ffmpeg", "-i", url, "-t", "00:00:01", "-f", "null", "-"],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                timeout=10,
+            )
+            return result.returncode == 0
+        except Exception as e:
+            print(f"Erro ao verificar o link: {url} -> {e}")
+            return False
